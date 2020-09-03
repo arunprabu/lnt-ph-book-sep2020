09efe99a7f8cd5eadb3f11a115b267cf94101d57
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-09-03 10:43:23 +0530

    prop binding

--- src/app/concepts/concepts.component.ts
+++ src/app/concepts/concepts.component.ts
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConceptsComponent implements OnInit {
 
+  // string interpolation related
+  appName = 'Phone Book App!';
+  exp = 20; // todos: work with boolean, array, obj
+
+  // property binding related
+  companyName = 'L & T Constructions';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getExp(): number {
+    return this.exp;
+  }
 }
