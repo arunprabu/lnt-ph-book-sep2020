a17e59c7786433b8ae9e7ea4d2f44b7c427aacad
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-09-07 18:21:42 +0530

    after pipes

--- src/app/shared/pipes/ellipsis.pipe.ts
+++ src/app/shared/pipes/ellipsis.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsisPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: string, ...args: any[]): unknown {
     console.log(args[0]);
     value = value.substring(0, args[0]);
     return value + '...';
