286d7daac2e71809f54d1ce4f73679b18e11ddad
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-09-07 11:00:36 +0530

    service any to any

---
+++ src/app/shared/services/account-data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountDataService {
+  // Step 1:
+  accountName = 'Arun';
+
+  constructor() { }
+}
